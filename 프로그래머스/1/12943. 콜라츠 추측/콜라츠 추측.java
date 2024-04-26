c27e8d23c3bac3cc7eb18fbb126acaf4d5601b13
JHeaon <83858260+JHeaon@users.noreply.github.com>
2024-04-26 14:41:30 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int num) {
+        int count = 0;
+        
+        while(count != 500){
+            
+            if(num == 1){
+                return count;
+            }
+            else if(num % 2 == 0){
+                num = num / 2;
+            }
+            
+            else if(num % 2 == 1){
+                num = num * 3 + 1;
+            }
+                   
+            count++;
+        }
+        
+        return -1;
+    }
+}
